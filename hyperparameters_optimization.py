5fee6feea9301217ca69f4a2dec1ab9ad4a97f0e
Lorenzo Squadrani <lorenzo.squadrani@gmail.com>
2022-07-18 14:33:14 +0200

    minor fix

--- hyperparameters_optimization.py
+++ hyperparameters_optimization.py
@@ -1,4 +1,5 @@
 import multiprocessing, psutil, os, time, sys, yaml
+import numpy as np
 import optuna
 
 from main import episode_run
@@ -12,11 +13,17 @@ with open(parameter_file, 'r') as stream:
         print(exc)
 
 
-def objective(trial):
+def objective_performance(trial):
 
+    """
+    Objective to maximize the probability of finding rewards in the last 5 trials.
+    """
+    
     conf['AC']['A_DA'] = trial.suggest_float('A_DA', 0.001, 0.1)
     conf['AC']['A_5HT'] = trial.suggest_float('A_5HT', 0.001, 0.1)
     conf['AC']['A_ACh'] = trial.suggest_float('A_ACh', 0.001, 0.1)
+    conf['AC']['w_max'] = trial.suggest_int('w_max_ac', 1, 10)
+    
 
     # Run all episodes
     pool = multiprocessing.Pool(os.cpu_count() - 1)
@@ -41,18 +48,17 @@ def objective(trial):
     # Compute average number of successive agents at the last trial
 
     counter = 0
-    success = 0
+    success = np.zeros(5)
     for result in results:
         
-        print(result['rewarding_trials'])
-        success += result['rewarding_trials'][-1]
+        success += result['rewarding_trials'][-5:]
         counter += 1
 
-    return 1 - success/counter
+    return 1 - (success/counter).mean()
 
 
 study = optuna.create_study()
-study.optimize(objective, n_trials=100)
+study.optimize(objective_performance, n_trials=100)
 
 print(study.best_params)  # E.g. {'x': 2.002108042}
 
